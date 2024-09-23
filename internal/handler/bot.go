fca37aad73b99e9c18711c8be65daad56eeb959f
asuma <770878450@qq.com>
2024-09-23 14:35:49 +0800

    修改email提示

--- internal/handler/bot.go
+++ internal/handler/bot.go
@@ -16,12 +16,20 @@ func Bot() *BotHandler {
 
 func (h *BotHandler) HealCheckCallback(resp openwechat.SyncCheckResponse) {
 	msg := fmt.Sprintf("HealCheck WARNING RetCode:%s Selector:%s", resp.RetCode, resp.Selector)
+	logPrint := false
 	if !resp.NorMal() && !resp.HasNewMessage() {
-		log.Println(msg)
+		logPrint = true
 	}
 
 	if !resp.Success() {
+		if resp.RetCode == "1102" {
+			msg += "\n 机器人可能已退出登录"
+		}
 		_ = service.NewEmail().Send(context.Background(), "WX-Bot 状态异常", msg)
+		logPrint = true
+	}
+
+	if logPrint {
 		log.Println(msg)
 	}
 }
