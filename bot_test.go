784ff5a92061913f04ca1572e363f2a289adb6b7
asuma <770878450@qq.com>
2024-09-24 13:58:30 +0800

    some

--- bot_test.go
+++ bot_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestAny(t *testing.T) {
-	//service.NewEmail().Send(context.TODO(), "WX-Bot 系统故障", "bot error: HEALTH CHECK WARING Retcode 1102 selecte:0 ")
+
 }
 
 func TestMainTest(t *testing.T) {
